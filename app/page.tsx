1add8b40e856abfeca92a088e79492e4cb1ca9f8
Richard Addo <ramankwah03@gmail.com>
2024-07-28 18:48:20 +0200

    test modifi

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
-import WebDesign from "@/components/details/web_design";
 import HomePage from "@/components/home/frame";
-import AppDesign from "@/components/details/app_design";
-import GraphicDesign from "@/components/details/graphic_design";
-import About from "@/components/about/about";
-import Location from "@/components/location/location";
-import Map from "@/components/map";
-import dynamic from "next/dynamic";
-import Contact from "@/components/contact/contact";
 
 export default function Home() {
   return (
