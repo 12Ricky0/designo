236ae5aab338b9d763ee544772e4701944f3a980
Richard Addo <ramankwah03@gmail.com>
2024-07-29 21:09:34 +0200

    test modi

--- __test__/contact.test.tsx
+++ __test__/contact.test.tsx
@@ -1,16 +1,2 @@
-import ContactForm from "@/components/contact/contact";
-import { render, screen } from "@testing-library/react";
-import { useFormState } from "react-dom";
 
-jest.mock("../lib/actions", () => {
-  const actual = jest.requireActual("../lib/actions");
-  return {
-    ...actual,
-    // useFormState: jest.fn(),
-  };
-});
 
-it("renders Contact page", () => {
-  render(<ContactForm />);
-  //   expect(getByText(/Contact/i)).toBeInTheDocument();
-});
--- __test__/location.test.tsx
+++ __test__/location.test.tsx
@@ -1,11 +1,21 @@
 import LocationMap from "@/components/location/location";
 import { screen, render } from "@testing-library/react";
+import Map from "@/components/map";
 
 it("renders LocationMap component", async () => {
   render(<LocationMap />);
-  await pause();
-  screen.debug();
-  //   expect(await screen.findByText(/Location/i)).toBeInTheDocument();
+  expect(await screen.findByText(/Location/i)).toBeInTheDocument();
+});
+
+jest.mock("../components/map", () => {
+  return jest.fn(() => <div>This is the map component</div>);
+});
+
+it("renders LocationMap component", async () => {
+  render(<LocationMap />);
+  expect(await screen.findAllByText(/This is the map component/i)).toHaveLength(
+    3
+  );
 });
 
 const pause = async () => {
